d6b4fd365c3c0e4707899c056e396aa06ffd4116
jianchao ci <961705621@qq.com>
2026-09-15 17:27:59 +0200

    Restore README setup notes and add IROS citation

--- src/install/install_miniconda.sh
+++
@@ -1,7 +0,0 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh
-bash Miniconda3-py38_4.12.0-Linux-x86_64.sh
-rm Miniconda3-py38_4.12.0-Linux-x86_64.sh
-eval "$(cat ~/.bashrc | tail -n +10)"
-conda deactivate
-conda config --set auto_activate_base false
-conda create --name av python=3.8.10
